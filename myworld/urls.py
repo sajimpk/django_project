efe641308987f8af9aaf572cab5ccf7c117deeb6
SAJIM Pk <68387969+Devils520@users.noreply.github.com>
2023-07-27 10:17:28 +0600

    Update urls.py

--- myworld/urls.py
+++ myworld/urls.py
@@ -6,7 +6,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact/',include('contact.urls')),
-    # path('insat',include('contact.urls')),
    path('',include('blog.urls')),
    path('about_us/',include('about.urls')),
 ]
